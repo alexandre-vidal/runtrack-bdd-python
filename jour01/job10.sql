ebeae1f49c3550f24b2894f36738ca2248e89bf7
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-01-30 15:33:48 +0100

    Job 10

---
+++ jour01/job10.sql
@@ -0,0 +1,8 @@
+id,nom,prenom,age,email
+2,Steak,Chuck,45,chuck.steak@laplateforme.io
+1,Spaghetti,Betty,23,betty.spaghetti@laplateforme.io
+5,Dupuis,Gertrude,20,gertrude.dupuis@laplateforme.io
+3,Doe,John,18,john.doe@lapalteforme.io
+4,Barnes,Binkie,16,binkie.barnes@laplateforme.io
+
+# SELECT * FROM etudiant ORDER BY age DESC;
