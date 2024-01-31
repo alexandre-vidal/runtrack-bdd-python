0d9dead16712666eee83e394cb2b7945f7fd3bfa
Alexandre Vidal <alexandre.vidal@laplateforme.io>
2024-01-31 14:44:05 +0100

    Job 18

---
+++ jour01/job18.sql
@@ -0,0 +1,8 @@
+id,nom,prenom,age,email
+1,Spaghetti,Betty,20,betty.spaghetti@laplateforme.io
+2,Steak,Chuck,45,chuck.steak@laplateforme.io
+4,Barnes,Binkie,16,binkie.barnes@laplateforme.io
+5,Dupuis,Gertrude,20,gertrude.dupuis@laplateforme.io
+6,Dupuis,Martin,18,martin.dupuis@laplteforme.io
+
+# DELETE FROM etudiant WHERE id = 3
